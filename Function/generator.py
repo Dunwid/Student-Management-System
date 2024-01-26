083ce75672a659b915017894dde1308c8ab96446
Aaron <117382876+Dunwid@users.noreply.github.com>
2024-01-26 16:40:12 +0800

    Working on additional info

--- Function/generator.py
+++ Function/generator.py
@@ -2,7 +2,27 @@ from faker import Faker
 import random
 fake = Faker()
 
+# Generate the list of students on list
+def generate(num, additional_info=False):
+    add = additional_info
+    student = []
+    students = []
+    if num is int:
+        if add:
+            generator(additional_info=True)
+        else:
+            for i in range(num):
+                student1 = {}
+                name, section, student_no = generator()
+                student1['name'] = name
+                student1['section'] = section
+                student1['student_no'] = student_no
+                i += 1
+                student.append(student1)
+    else:
+        raise ValueError('Enter the Number of Students')
 
+# Generate names, student,
 def generator(additional_info=False):
     if additional_info:
         print('hey!')
