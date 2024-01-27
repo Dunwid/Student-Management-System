50359dbcd6c5f7b370bd6918ae9988cb3c1d823c
Aaron <117382876+Dunwid@users.noreply.github.com>
2024-01-27 21:10:01 +0800

    Update Menu.py

--- Function/Menu.py
+++ Function/Menu.py
@@ -1,13 +1,57 @@
 from tabulate import tabulate
 
+
 def main():
+    enter_key()
+
+
+def enter_key():
     menu()
+    while True:
+        try:
+            button = input('Enter key: ').upper()
+            match button:
+                case 'G':
+                    print("Monday")
+                case 'I':
+                    print("Tuesday")
+                case 'S':
+                    print("Wednesday")
+                case 'R':
+                    print("Thursday")
+                case 'U':
+                    print("Friday")
+                case 'D':
+                    print("Saturday")
+                case 'X':
+                    break
+                case _:
+                    raise ValueError
+        except ValueError:
+            print('Invalid KEY')
+    members()
+
 
 def menu():
-    headers = ["MENU"]
-    table = [['G', 'Generate Students'], ['I', 'Input Students'], ['C', 'From CSV'],
-             ['R', 'See Students'], ['U', 'Update Students'], ['D', 'Delete Students']]
+    table = [['G', 'Generate Students'], ['I', 'Input Students'], ['S', 'Save'],
+             ['R', 'See Students'], ['U', 'Update Students'], ['D', 'Delete Students'],
+             ['X', 'Exit Program']
+             ]
     print("----------MENU-----------")
     print(tabulate(table, tablefmt="grid"))
-    key = input('Enter key: ')
-main()
+
+
+def members():
+    return print(""""
+    STUDENT MANAGEMENT SYSTEM
+    BSCS 2B PROJECT
+
+    MEMBERS:
+        Malinis, Johnbert
+        Mayor, Mark Aaron
+        Pula, Henry Luis
+        """)
+
+
+if __name__ == "__main__":
+    main()
